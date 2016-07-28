4fbd07873e68186bff20af8a57a940ecfb28df8d
Corw <pzamyatin@gmail.com>
2016-07-28 18:48:20 +1200

    Added bubble_sort

---
+++ 07.28.01_bubble_sort.py
@@ -0,0 +1,38 @@
+import random, time
+
+unsorted_list = [random.randrange(0,99999) for i in range(0,99999)]
+random.shuffle(unsorted_list)
+
+print('Unsorted list = ', unsorted_list)
+
+def decorator_time(func):
+
+    def wrapper(*args, **kwargs):
+        
+        start = time.time()
+        lst = func(*args, **kwargs)
+        stop = time.time() - start
+
+        return lst, stop
+
+    return wrapper
+
+@decorator_time
+def bubble_sort(lst):
+    
+    for x in range(len(lst)-1):
+        
+        for i in range(len(lst)-1):
+
+            tmp = lst[i]
+            
+            if lst[i] > lst[i+1]:
+                lst[i] = lst[i+1]
+                lst[i+1] = tmp
+
+    return lst
+
+sorter_list, sec = bubble_sort(unsorted_list)
+
+print('Sorted list:', sorter_list)
+print('Time to sort:', round(sec, 3), 'sec')
