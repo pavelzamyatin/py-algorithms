9ff872ca69044e9728f6d0454273cb95d8691631
Corw <pzamyatin@gmail.com>
2016-08-02 15:59:19 +1200

    Added shaker sort, fixed bubble sort

--- 04_bubble_sort.py
+++ 04_bubble_sort.py
@@ -1,9 +1,9 @@
 import random, time
 
-unsorted_list = [random.randrange(0,9999) for i in range(0,9999)]
+unsorted_list = [random.randrange(0,99) for i in range(0,99)]
 random.shuffle(unsorted_list)
 
-print('Unsorted list = ', unsorted_list)
+#print('Unsorted list = ', unsorted_list)
 
 def decorator_time(func):
 
@@ -20,19 +20,50 @@ def decorator_time(func):
 @decorator_time
 def bubble_sort(lst):
     
-    for x in range(len(lst)-1):
-        
-        for i in range(len(lst)-1):
+    left = 0
+    right = len(lst)-1
 
-            tmp = lst[i]
+    while left <= right:
+        
+        for i in range(left, right, +1):
             
             if lst[i] > lst[i+1]:
-                lst[i] = lst[i+1]
-                lst[i+1] = tmp
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+
+        right -= 1
 
     return lst
 
-sorter_list, sec = bubble_sort(unsorted_list)
+@decorator_time
+def shaker_sort(lst):
 
-print('Sorted list:', sorter_list)
-print('Time to sort:', round(sec, 3), 'sec')
+    left = 0
+    right = len(lst)-1
+
+    while left <= right:
+
+        for i in range(left, right, +1):
+            
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+
+        right -= 1
+
+        for y in range(left,right, -1):
+
+            if lst[y-1] > lst[y]:
+                lst[y], lst[y-1] = lst[y-1], lst[y]
+
+        left += 1
+
+    return lst
+
+bubble_sorted_list, sec1 = bubble_sort(unsorted_list)
+shaker_sorted_list, sec2 = shaker_sort(unsorted_list)
+
+
+print('[BUBLE ] Sorted list:', bubble_sorted_list)
+print('[SHAKER] Sorted list:', shaker_sorted_list)
+
+print('[BUBLE ] Time to sort:', round(sec1, 6), 'sec')
+print('[SHAKER] Time to sort:', round(sec2, 6), 'sec')
